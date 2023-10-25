8f2c6e4d40435a3e301e99725d775e79bc2f6f92
Bero000 <122268572+Bero000@users.noreply.github.com>
2023-10-25 18:39:22 +0300

    Update ft_rrange.c

--- Level 3/ft_rrange/ft_rrange.c
+++ Level 3/ft_rrange/ft_rrange.c
@@ -1,9 +1,16 @@
 #include <stdlib.h>
 
+int to_abs(int c)
+{
+	if (c < 0)
+		return (-c);
+	return c;
+}
+
 int	*ft_rrange(int start, int end)
 {
     int i = 0;
-    int *res = (int*)malloc(sizeof(int) * abs(start - end) + 1);
+    int *res = (int*)malloc(sizeof(int) * to_abs(start - end) + 1);
 
     while(start < end)
     {
@@ -35,10 +42,17 @@ int	*ft_rrange(int start, int end)
 
 #include <stdlib.h>
 
+int to_abs(int c)
+{
+	if (c < 0)
+		return (-c);
+	return c;
+}
+
 int     *ft_rrange(int start, int end)
 {
 	int i = 0;
-	int len = abs(start - end);
+	int len = to_abs(start - end);
 	int *res= (int *)malloc(sizeof(int) * len);
 
 	if (!res)
