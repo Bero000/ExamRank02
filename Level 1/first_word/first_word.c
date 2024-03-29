d6a9b9eb047555f7507735c58c4c2e27e569c25c
Bero000 <122268572+Bero000@users.noreply.github.com>
2024-03-29 20:43:10 +0300

    Update first_word.c

--- Level 1/first_word/first_word.c
+++ Level 1/first_word/first_word.c
@@ -5,9 +5,9 @@ int main(int ac, char **av)
     int i = 0;
     if(ac == 2)
     {
-        while(av[1][i] <= 32)
+        while(av[1][i] <= 32 && av[1][i])
             i++;
-        while(av[1][i] > 32)
+        while(av[1][i] > 32 && av[1][i])
             write(1, &av[1][i++], 1);
     }   
     write(1, "\n", 1);
